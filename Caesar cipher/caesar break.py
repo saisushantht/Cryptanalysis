3465c234edd148997be819b69f43e884e274efc1
Thakkalapally Sai Sushanth <47238397+saisushantht@users.noreply.github.com>
2020-08-05 22:41:40 +0530

    Update caesar break.py

--- Caesar cipher/caesar break.py
+++ Caesar cipher/caesar break.py
@@ -1,7 +1,7 @@
 def decrypt(string, shift):
   plain = ''                                                     #intilizing string to store cipher text
   for char in string:                                             
-    if char == ' ':                                                #to add space as it is
+    if char.isalpha()==False:                                                #to add chars other than alphabets as it is
       plain = plain + char                                       
     elif  char.isupper():                                          #to apply the encryption on the upper case letters 
       plain = plain + chr((ord(char) - shift - 65) % 26 + 65)    #for upper case letters to shift eith ASCII values 
